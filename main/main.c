13e46b9c3e627e13852e1711ab9e75845acc57eb
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2016-11-05 19:15:33 +0100

    Added some register info to rom dump

--- main/main.c
+++ main/main.c
@@ -17,13 +17,30 @@
 /* For testing of how code is generated  */
 #if 0
 /*         "wsr 	a3, ps\n"\ */
+         "movi.n	a2, 0x3ffe0400\n"
+         "movi.n	a3,0x400807BC\n"
+        "movi.n	a2, 0x3ffe0400\n"\
+        "movi.n	a3, 0x400807BC\n"\
+        "s32i.n        a2, a3 , 0\n"\
+
+#endif
+
+	 // Load a9 with adress to user_entr  (+20)
+	 // Load a8 with adress to elf_entry  (+40)
+void patch() {
+    asm volatile (\
+        "j      0x40007023\n"\
+	"nop.n\n"\
+	"nop\n"\
+	"l32r     a9,0x3ffe040\n"\
+	"l32r     a8,0x40\n"\
+        "s32i.n   a9,a8,0\n"\
+	"movi.n	a2, 0\n");
+}
 
 void retint() {
-    asm volatile (\
-        "wsr 	a3 , ps\n"\
-        "movi.n	a8, -9\n"\
-        "and	a2, a2, a8\n"\
-        "RFDE\n");
+      unsigned int*mem_location=(unsigned int*)0x40000000;
+      *mem_location=0x3ffe0400;
 }
 
 int test(int in) {
@@ -36,7 +53,7 @@ void jump() {
          "jx a0\n");
     retint();
 }
-#endif
+
 
 /* produce simple hex dump output */
 void simpleHex(int toOutout)
@@ -71,6 +88,7 @@ void dump_task(void *pvParameter)
     unsigned int*simple_mem_location=(unsigned int*)mem_location;
     unsigned int* end=(unsigned int*)0x400C1FFF;
 
+
     printf("\n");
 
     int *GPIO_STRAP_TEST=(int *)0x3ff44038;
@@ -82,13 +100,29 @@ void dump_task(void *pvParameter)
     //RTC_CNTL_RESET_STATE_REG  3ff48034
 
     test=(int *)RTC_CNTL_INT_ST_REG;
-    printf( "RTC_CNTL_INT_ST_REG;,%08X=%08X\n",RTC_CNTL_INT_ST_REG, *test);
+    printf( "RTC_CNTL_INT_ST_REG; %08X=%08X\n",RTC_CNTL_INT_ST_REG, *test);
 
     test=(int *)0x3FF00044;
-    printf( "DPORT 44 REG;,%08X=%08X\n",0x3FF00044, *test);
+    printf( "DPORT 44 REG; %08X=%08X\n",0x3FF00044, *test);
 
     test=(int *)0x3FF00040;
-    printf( "DPORT 40 REG;,%08X=%08X\n",0x3FF00040, *test);
+    printf( "DPORT 40 REG; %08X=%08X\n",0x3FF00040, *test);
+
+
+    test=(int *)0x3FF003f0;
+    printf( "DPORT DPORT_PRO_DCACHE_DBUG0_REG REG; %08X=%08X\n",0x3FF003f0, *test);
+
+
+
+    test=(int *)0x3FF00038;
+    printf( "DPORT 38 REG; %08X=%08X\n",0x3FF00038, *test);
+
+    test=(int *)0x3FF4800c;
+    printf( "DPORT RTC_CNTL_TIME_UPDATE_REG REG; %08X=%08X\n",0x3FF4800c, *test);
+
+
+
+
 
     printf("ROM DUMP\n");
     printf("\n");
@@ -141,6 +175,8 @@ void dump_task(void *pvParameter)
     }
     fflush(stdout);
 
+
+
     printf("\n");
     printf("Done\n");
     printf("\n");
@@ -150,6 +186,8 @@ void dump_task(void *pvParameter)
     while (true) {
         gpio_set_level(GPIO_NUM_4, level);
         level = !level;
+	printf("blink\n");
+        printf( "DPORT RTC_CNTL_TIME_UPDATE_REG REG; %08X=%08X\n",0x3FF00038, *test);
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
     //system_restart();
@@ -157,7 +195,7 @@ void dump_task(void *pvParameter)
 
 void app_main()
 {
-    nvs_flash_init();
+    //nvs_flash_init();
     system_init();
     xTaskCreate(&dump_task, "dump_task", 2048, NULL, 5, NULL);
 }
