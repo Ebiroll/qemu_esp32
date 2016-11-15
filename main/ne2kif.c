35bc713d273faa0e9c49ac8f781ec10978379e18
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2016-11-16 00:18:13 +0100

    Added irq example

--- main/ne2kif.c
+++ main/ne2kif.c
@@ -1154,6 +1154,7 @@ static void low_level_init(struct netif * netif)
 	printf("%08X\n",test);
     //test=EN0_MIISTATUS;
 
+    //static void IRAM_ATTR frc_timer_isr()
 	// intr_matrix_set(xPortGetCoreID(), ETS_TIMER1_INTR_SOURCE, ETS_FRC1_INUM);
     // xt_set_interrupt_handler(ETS_FRC1_INUM, &frc_timer_isr, NULL);                                                           
     // xt_ints_on(1 << ETS_FRC1_INUM);                                   
