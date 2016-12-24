b6f79f139ad66ad0b95b542e8ae144563d88fd64
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2016-12-24 14:48:51 +0100

    Added screen-dump of partion table on hardware

--- examples/07_flash_test/main/app_main.c
+++ examples/07_flash_test/main/app_main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include "esp_partition.h"
 #include "esp_flash_data_types.h"
+#include "nvs_flash.h"
 
 static uint32_t g_wbuf[SPI_FLASH_SEC_SIZE / 4];
 static uint32_t g_rbuf[SPI_FLASH_SEC_SIZE / 4];
@@ -97,12 +98,54 @@ void mmapPartitionData() {
     void *ptr;
     spi_flash_mmap_handle_t handle;
 
-   esp_err_t err = spi_flash_mmap(ESP_PARTITION_TABLE_ADDR & 0xffff0000,
+    printf("mmap partition data\n");
+
+    esp_err_t err = spi_flash_mmap(ESP_PARTITION_TABLE_ADDR & 0xffff0000,
             SPI_FLASH_SEC_SIZE, SPI_FLASH_MMAP_DATA, (const void**) &ptr, &handle);
+
+    //esp_err_t err = spi_flash_mmap(ESP_PARTITION_TABLE_ADDR,
+    //        SPI_FLASH_SEC_SIZE, SPI_FLASH_MMAP_DATA, (const void**) &ptr, &handle);
     if (err != ESP_OK) {
-        return err;
+      printf("mmap fail\n");
+      //return err;
     }
- 
+
+    printf("mmap success\n");
+
+    //int *test=(int *)*ptr;
+    //printf( "%08X\n",(int )test);
+    unsigned int *iptr=(unsigned int *)ptr;
+    // Dump entire sector
+    int q=0;
+    int j=0;
+    for(q=0;q<1024;q++) 
+    {
+        g_rbuf[q]=*iptr++;
+        printf( "%08X", g_rbuf[q]);
+        if (q%8==7) {
+            printf("  ");
+            int letters[16];
+            // Only 4 byte access?
+            //memcpy(letters,g_rbuf[q-6],8*4);
+            letters[0]=g_rbuf[q-7];
+            letters[1]=g_rbuf[q-6];
+            letters[2]=g_rbuf[q-5];
+            letters[3]=g_rbuf[q-4];
+            letters[4]=g_rbuf[q-3];
+            letters[5]=g_rbuf[q-2];
+            letters[6]=g_rbuf[q-1];
+            letters[7]=g_rbuf[q];
+
+            char *text=(char *)letters;
+            for(j=0;j<8*4;j++) {
+                if (printable(*text)) {
+                     printf( "%c", *text);
+                }
+                text++;
+            }            
+            printf("\n");
+        }
+    }
 
 
 }
@@ -164,13 +207,18 @@ void readWriteTask(void *pvParameters)
 void app_main()
 {
     // workaround: configure SPI flash size manually (2nd argument)
-    SPIParamCfg(0x1540ef, 4*1024*1024, 64*1024, 4096, 256, 0xffff);
+    //SPIParamCfg(0x1540ef, 4*1024*1024, 64*1024, 4096, 256, 0xffff);
+    nvs_flash_init();
 
     // This is just a qemu trick to restore original rom content 
-    int *unpatch=(int *) 0x3ff00088;
-    *unpatch=0x42;
+    //int *unpatch=(int *) 0x3ff00088;
+    //*unpatch=0x42;
+
+    spi_flash_mmap_dump();
 
     dumpPartitionData();
+
+    mmapPartitionData();
 
     spi_flash_mmap_dump();
 
