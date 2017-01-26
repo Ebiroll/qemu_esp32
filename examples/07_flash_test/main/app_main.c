bc3f4c391983528ffdbc12f4a0377dbaaee5fc18
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2017-01-26 06:49:49 +0100

    More flash write tests

--- examples/07_flash_test/main/app_main.c
+++ examples/07_flash_test/main/app_main.c
@@ -53,7 +53,7 @@ void dumpPartitionData() {
         it++;
     }
 
-    // Dump entire sector
+    // Dump beginning of sector
     int q=0;
     int j=0;
     for(q=0;q<1024;q++) 
@@ -86,7 +86,7 @@ void dumpPartitionData() {
 
 
 
-/* WTF?
+/* Original 
     esp_err_t err = spi_flash_mmap(ESP_PARTITION_TABLE_ADDR & 0xffff0000,
             SPI_FLASH_SEC_SIZE, SPI_FLASH_MMAP_DATA, (const void**) &ptr, &handle);
     if (err != ESP_OK) {
@@ -101,6 +101,64 @@ void dumpPartitionData() {
 
 
 }
+
+void dumpSector0() {
+
+    spi_flash_read(0 , g_rbuf, sizeof(g_rbuf));
+    //esp_partition_t* p = (esp_partition_t*) g_rbuf;
+
+    printf("Dump of sector 0, sectors are (4096) 0x1000 bytes\n");
+    printf("Pages are (65536) 0x10000 bytes = (16) 0x10 sectors long\n");
+
+    // Dump entire sector
+    int q=0;
+    int j=0;
+    for(q=0;q<SPI_FLASH_SEC_SIZE/4;q++) 
+    {
+        printf( "%08X", g_rbuf[q]);
+        if (q%8==7) {
+            printf("  ");
+            int letters[16];
+            // Only 4 byte access?
+            //memcpy(letters,g_rbuf[q-6],8*4);
+            letters[0]=g_rbuf[q-7];
+            letters[1]=g_rbuf[q-6];
+            letters[2]=g_rbuf[q-5];
+            letters[3]=g_rbuf[q-4];
+            letters[4]=g_rbuf[q-3];
+            letters[5]=g_rbuf[q-2];
+            letters[6]=g_rbuf[q-1];
+            letters[7]=g_rbuf[q];
+
+            char *text=(char *)letters;
+            for(j=0;j<8*4;j++) {
+                if (printable(*text)) {
+                     printf( "%c", *text);
+                }
+                text++;
+            }            
+            printf("\n");
+        }
+    }
+
+
+
+/* Original 
+    esp_err_t err = spi_flash_mmap(ESP_PARTITION_TABLE_ADDR & 0xffff0000,
+            SPI_FLASH_SEC_SIZE, SPI_FLASH_MMAP_DATA, (const void**) &ptr, &handle);
+    if (err != ESP_OK) {
+        return err;
+    }
+    // calculate partition address within mmap-ed region
+    const esp_partition_info_t* it = (const esp_partition_info_t*)
+            (ptr + (ESP_PARTITION_TABLE_ADDR & 0xffff) / sizeof(*ptr));
+    const esp_partition_info_t* end = it + SPI_FLASH_SEC_SIZE / sizeof(*it);
+
+*/
+
+
+}
+
 
 
 void mmapPartitionData() {
@@ -174,13 +232,13 @@ void readWriteTask(void *pvParameters)
 {
     //srand(0);
 
-
+    dumpSector0();
 
     //    for (uint32_t base_addr = 0x200000;
     //     base_addr < 0x300000;
     //     base_addr += SPI_FLASH_SEC_SIZE)
         for (uint32_t base_addr = 0x000000;
-         base_addr < 0x8000;
+         base_addr < 0x1000;
          base_addr += SPI_FLASH_SEC_SIZE)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -215,6 +273,9 @@ void readWriteTask(void *pvParameters)
     // TODO flash map and check data.
     //spi_flash_mmap
 
+    dumpSector0();
+
+
     spi_flash_mmap_dump();
 
 
@@ -228,14 +289,23 @@ void readWriteTask(void *pvParameters)
 
 void mmu_table_dump()
 {
-    uint32_t test;    
-    for (int i = 0; i < REGIONS_COUNT * PAGES_PER_REGION; ++i) {
+    uint32_t test;  
+    int i;  
+    for (i = 0; i < REGIONS_COUNT * PAGES_PER_REGION; ++i) {
         test=DPORT_PRO_FLASH_MMU_TABLE[i];
 
         if (test!=0) {
-            printf("page %d: paddr=%08X\n",i, test);
+            printf("pro page %d: paddr=%08X\n",i, test);
         }
     }
+    // DPORT_APP_FLASH_MMU_TABLE
+    for (i = 0; i < REGIONS_COUNT * PAGES_PER_REGION; ++i) {
+        test=DPORT_APP_FLASH_MMU_TABLE[i];
+        if (test!=0) {
+            printf("app page %d: paddr=%08X\n",i, test);
+        }
+    }
+
 }
 
 
