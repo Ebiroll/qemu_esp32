a622b9a1ccc7b645d4b6e1d20b54b7bbdf790571
Olof Astrand <olof.astrand@siatm.com>
2017-02-14 16:08:41 +0000

    Added i2cscan

---
+++ examples/10_i2cscan/main/app_main.c
@@ -0,0 +1,131 @@
+/*
+ *
+ * Written by Olof Astrand <olof.astrand@gmail.com>
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ *
+*/
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+
+
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "driver/i2c.h"
+
+
+// Use same inputs as sparkfun
+
+#define I2C_MASTER_SCL_IO    22    /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO    21    /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM   I2C_NUM_0   /*!< I2C port number for master dev */
+#define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_FREQ_HZ    100000     /*!< I2C master clock frequency */
+
+#define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL    0x0         /*!< I2C ack value */
+#define NACK_VAL   0x1         /*!< I2C nack value */
+
+
+
+static const char *TAG = "i2cscan";
+
+
+/**
+ * @brief test function to show buffer
+ */
+void show_buf(uint8_t* buf, int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%02x ", buf[i]);
+        if (( i + 1 ) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+/**
+ *
+ * _______________________________________________________________________________________
+ * | start | slave_addr  +ack | 
+ * --------|--------------------------|----------------------|--------------------|------|
+ *
+ */
+esp_err_t i2c_master_check_slave(i2c_port_t i2c_num,uint8_t addr)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( addr << 1 ) , ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+
+/**
+ * @brief i2c master initialization
+ */
+void i2c_init()
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+
+/**
+ * @brief Scan all i2c adresses 
+ */
+void i2c_scan() {
+	int address;
+    int ret;
+	int foundCount = 0;
+	for (address=1; address<127; address++) {
+        ret=i2c_master_check_slave(I2C_MASTER_NUM,address);
+        if (ret == ESP_OK) {
+            printf("Found device addres: %02x\n", address);
+            foundCount++;
+        }
+    }
+    printf("Done scanning.. found %d devices\n",foundCount);
+}
+
+
+
+void app_main()
+{
+    printf("Starting i2cscan... \r\n");
+
+    i2c_init();
+    i2c_scan();
+
+}
