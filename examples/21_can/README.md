d6abfaa0f6bb93e47bf8adb966e65ec73bb02459
Olof Astrand <olof.astrand@siatm.com>
2017-03-22 09:06:05 +0000

    Can driver added

---
+++ examples/21_can/README.md
@@ -0,0 +1,12 @@
+
+Hardware should be SJA1000 compatible.
+
+https://www.nxp.com/documents/data_sheet/SJA1000.pdf
+
+# ESP32-CAN-Driver
+CAN driver project for the Espressif ESP32
+
+Download and install the ESP IDF framework from https://github.com/espressif/esp-idf. The CAN project works the same way as the other example projects.
+
+More Infos here:
+http://www.barth-dev.de/can-driver-esp32/
---
+++ examples/21_can/main/drv/CAN/cfg/CAN_config.c
@@ -0,0 +1,37 @@
+/**
+ * @section License
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017, Thomas Barth, barth-dev.de
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "CAN_config.h"
+
+
+CAN_device_t CAN_cfg = {
+	.speed=CAN_SPEED_1000KBPS,		// CAN Node baudrade
+	.tx_pin_id = GPIO_NUM_5,		// CAN TX pin
+	.rx_pin_id = GPIO_NUM_4,		// CAN RX pin
+	.rx_queue=NULL,					// FreeRTOS queue for RX frames
+};
