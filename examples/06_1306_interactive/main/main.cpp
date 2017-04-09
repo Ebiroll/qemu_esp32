56e60802338559211ce83c070e2f9845c25485ed
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2017-04-09 11:06:25 +0200

    Interactive 1306 example

---
+++ examples/06_1306_interactive/main/main.cpp
@@ -0,0 +1,215 @@
+//
+// Lots of this based on ncolbans work
+// https://github.com/nkolban/esp32-snippets
+/*
+MIT License
+
+Copyright (c) 2017 Olof Astrand (Ebiroll)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include <sys/time.h>
+
+static const char *TAG = "interact";
+
+
+
+// readline from readLine.c
+extern "C" char *readLine(uart_port_t uart,char *line,int len);
+extern "C" char *pollLine(uart_port_t uart,char *line,int len);
+
+
+
+#define BUF_SIZE 512
+
+char echoLine[512];
+
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "ssid",
+            .password = "password",
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+// Similar to uint32_t system_get_time(void)
+uint32_t get_usec() {
+
+ struct timeval tv;
+
+ //              struct timeval {
+ //                time_t      tv_sec;     // seconds
+ //                suseconds_t tv_usec;    // microseconds
+ //              };
+
+ gettimeofday(&tv,NULL);
+ return (tv.tv_sec*1000000 + tv.tv_usec);
+
+
+  //uint64_t tmp=get_time_since_boot();
+  //uint32_t ret=(uint32_t)tmp;
+  //return ret;
+}
+
+
+char line[256];
+
+void printMenu {
+    printf("1. Scan i2c bus.\n");
+    printf("2. Ultrasound measure.\n");
+    printf("3. Echo to UART 2, add crlf before send.\n");
+    printf("4. Try connecting over gprs with A6Lib.\n");
+    printf("5. Try send data over gprs with A6Thingspeak.\n");
+}
+
+//
+// Menu from uart 1
+//
+static void uartLoopTask(void *inpar)
+{
+    bool ultraDistanceRunning = false;
+    QueueHandle_t uart_queue;
+
+    // Could not get to work with UART0 
+    uart_port_t uart_num = UART_NUM_1; // uart port number
+    uart_config_t uart_config = {
+        .baud_rate = 115200,                   //baudrate
+        .data_bits = UART_DATA_8_BITS,         //data bit mode
+        .parity = UART_PARITY_DISABLE,         //parity mode
+        .stop_bits = UART_STOP_BITS_1,         //stop bit mode
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, //hardware flow control(cts/rts)
+        .rx_flow_ctrl_thresh = 122,            //flow control threshold
+    };
+    ESP_LOGI(TAG, "Setting UART configuration number %d...", uart_num);
+
+    // Should be set
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, 23, 19, -1, -1));
+
+    // driver_install, otherwise E (20206) uart: uart_read_bytes(841): uart driver error
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, 512 * 2, 512 * 2, 10, &uart_queue, 0));
+    sprintf(line,"Select 1,2,3,4 or 5\n");
+    uart_tx_chars(UART_NUM_1, (const char *)line, strlen(line));
+    
+    printf("1. Menu\n");
+
+    //#if 0
+    char *data;
+
+    while (true)
+    {
+        data = pollLine(uart_num, line, 256);
+        if (strcmp(data, "1") == 0)
+        {
+            printf("Menu\n");
+            I2CScanner();
+        }
+        else if (strcmp(data, "2") == 0)
+        {
+
+        }
+        else if (strcmp(data, "3") == 0)
+        {
+        }
+        else if (strcmp(data, "4") == 0)
+        {
+        }
+        else if (strcmp(data, "5") == 0)
+        {
+        }
+        else
+        {
+
+        }
+    }
+    //#endif
+}
+
+extern "C" void app_main(void)
+{
+    nvs_flash_init();
+
+    //Serial.begin(115200);
+
+    // Note!! Sonar also uses UART 1
+    //xTaskCreatePinnedToCore(&maxSonarInput, "sonar", 8048, NULL, 5, NULL, 0);
+
+    // Continusly transmit on uart2, (for qemu tests)
+    //xTaskCreatePinnedToCore(&uartTestTask, "uart", 8048, NULL, 5, NULL, 0);
+
+    //xTaskCreatePinnedToCore(&A6TestTask, "A6", 8048, NULL, 5, NULL, 0);
+    //xTaskCreatePinnedToCore(&ultraDistanceTask, "ultra", 4096, NULL, 20, NULL, 0);
+
+    xTaskCreatePinnedToCore(&uartLoopTask, "loop", 4096, NULL, 20, NULL, 0);
+
+
+#if 0
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = "access_point_name",
+            .password = "password",
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+#endif
+
+
+// 
+
+
+
+
+}
