776755f28aa4eda260e019016865350c3a84fdcf
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2017-08-04 14:29:49 +0200

    Added patch in toflash

--- examples/23_ota/toflash.c
+++ examples/23_ota/toflash.c
@@ -9,12 +9,13 @@
 // 0x8000 /home/olas/esp/esp-idf/examples/system/ota/build/partitions_two_ota.bin
 //
 
-void merge_flash(char *binfile,char *flashfile,int flash_pos)
+void merge_flash(char *binfile,char *flashfile,int flash_pos,int patch_hash)
 {
     FILE *fbin;
     FILE *fflash;
     unsigned char *tmp_data;
-    unsigned char *flash_data;
+    int j=0;
+
 
     int file_size=0;
     int flash_size=0;
@@ -46,13 +47,8 @@ void merge_flash(char *binfile,char *flashfile,int flash_pos)
     rewind(fflash);
     fseek(fflash,flash_pos,SEEK_SET);
 
-    //flash_data=malloc((1+flash_size)*sizeof(char));    
-    //int len_read=fread(tmp_data,sizeof(char),file_size,fbin);
 
     tmp_data=malloc((1+file_size)*sizeof(char));
-    //size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
-    //size_t fwrite(const void *ptr, size_t size, size_t nmemb,
-    //                 FILE *stream);
 
     if (file_size<=0) {
       printf("Not able to get file size %s",binfile);
@@ -60,6 +56,14 @@ void merge_flash(char *binfile,char *flashfile,int flash_pos)
 
     int len_read=fread(tmp_data,sizeof(char),file_size,fbin);
 
+    if (patch_hash==1) {
+      for (j=0;j<33;j++)
+	{
+          tmp_data[file_size-j]=0;
+	}
+    }
+
+    
     int len_write=fwrite(tmp_data,sizeof(char),file_size,fflash);
 
     if (len_read!=len_write) {
@@ -81,12 +85,17 @@ int main(int argc,char *argv[])
 {
     // system("cp esp32flash.bin esp32flash.bak");
 
-    // Add bootloader
-    merge_flash("build/bootloader/bootloader.bin","esp32flash.bin",0x1000);
-    // Add partitions, test OTA here
-    merge_flash("build/partitions_two_ota.bin","esp32flash.bin",0x8000);
-    // Add application
-    merge_flash(argv[1],"esp32flash.bin",0x10000);
+    // Overwrites esp32flash.bin file
+    system("dd if=/dev/zero bs=1M count=4  | tr \"\\000\" \"\\377\" >  esp32flash.bin");
 
+
+    // Add bootloader
+    merge_flash("build/bootloader/bootloader.bin","esp32flash.bin",0x1000,0);
+    // Add partitions, test OTA here
+    merge_flash("build/partitions_two_ota.bin","esp32flash.bin",0x8000,0);
+    // Add application
+    merge_flash(argv[1],"esp32flash.bin",0x10000,1);
+
+    system("cp esp32flash.bin ~/qemu_esp32");
 }
 
