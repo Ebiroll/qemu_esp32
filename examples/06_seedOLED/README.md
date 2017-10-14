c09a68acb9202608defe623edd0b228b2392767d
Olof Astrand <olof.astrand@gmail.com>
2017-10-14 15:51:42 +0200

    Added simple hello world example

--- examples/06_seedOLED/README.md
+++ examples/06_seedOLED/README.md
@@ -2,6 +2,27 @@ Seed studio 12864 OLED,
 Probably a SSG1106 controller chip.
 This is for future emulation of 1106
 
+mkdir -p components && \
+cd components && \
+git clone https://github.com/espressif/arduino-esp32.git arduino && \
+cd .. && \
+make menuconfig
+
+Select     "Autostart Arduino setup and loop on boot"
+
+If you get this,
+ (0) flash_parts: partition 3 invalid - offset 0x150000 size 0x140000 exceeds flash chip size 0x200000
+E (0) boot: Failed to verify partition table
+E (0) boot: load partition table error!
+user code done
+
+ Serial flasher config  ---> 
+   Flash size (4 MB)  --->        
+
+
+
+
+
 SSG1106 compatible with SSD1306 basic, difference is that SSH1106 control chip RAM space is 132*64, while SSD1306 space is 128*64.
 
 The 1.3-inch OLED 128*64 dot matrix, so in the middle of the screen production took 128 row. When using SSD1306 program point SSH1106 screen, only need to change address to 0x02 row to start.
--- examples/06_seedOLED/main/SeeedOLED.cpp
+++ examples/06_seedOLED/main/SeeedOLED.cpp
@@ -1,28 +1,31 @@
 /*
-  SeeedOLED.cpp - SSD130x OLED Driver Library
-  2011 Copyright (c) Seeed Technology Inc.  All right reserved.
- 
-  Author: Visweswara R
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * SeeedOLED.cpp
+ * SSD130x OLED Driver Library
+ *
+ * Copyright (c) 2011 seeed technology inc.
+ * Author        :   Visweswara R
+ * Create Time   :   Dec 2011
+ * Change Log    :
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "Wire.h"
 #include "SeeedOLED.h"
 
-#include <avr/pgmspace.h>
+//#include <avr/pgmspace.h>
 
 // 8x8 Font ASCII 32 - 127 Implemented
 // Users can modify this to support more characters(glyphs)
@@ -126,15 +129,15 @@ const unsigned char BasicFont[][8] PROGMEM=
   {0x00,0x00,0x7F,0x00,0x00,0x00,0x00,0x00},
   {0x00,0x41,0x36,0x08,0x00,0x00,0x00,0x00},
   {0x00,0x02,0x01,0x01,0x02,0x01,0x00,0x00},
-  {0x00,0x02,0x05,0x05,0x02,0x00,0x00,0x00} 
+  {0x00,0x02,0x05,0x05,0x02,0x00,0x00,0x00}
 };
 
 void SeeedOLED::init(void)
 {
-    sendCommand(SeeedOLED_Display_Off_Cmd); 	//display off
-    delay(5); 
-    sendCommand(SeeedOLED_Display_On_Cmd); 	//display on
-    delay(5); 
+    sendCommand(SeeedOLED_Display_Off_Cmd);     //display off
+    delay(5);
+    sendCommand(SeeedOLED_Display_On_Cmd);  //display on
+    delay(5);
     sendCommand(SeeedOLED_Normal_Display_Cmd);  //Set Normal Display (default)
 }
 
@@ -142,13 +145,13 @@ void SeeedOLED::sendCommand(unsigned char command)
 {
   Wire.beginTransmission(SeeedOLED_Address); // begin I2C communication
 #if defined(ARDUINO) && ARDUINO >= 100
-  Wire.write(SeeedOLED_Command_Mode);	     // Set OLED Command mode
+  Wire.write(SeeedOLED_Command_Mode);        // Set OLED Command mode
   Wire.write(command);
 #else
-  Wire.send(SeeedOLED_Command_Mode);	     // Set OLED Command mode
+  Wire.send(SeeedOLED_Command_Mode);         // Set OLED Command mode
   Wire.send(command);
 #endif
-  Wire.endTransmission();    		     // End I2C communication
+  Wire.endTransmission();                // End I2C communication
 }
 
 void SeeedOLED::setBrightness(unsigned char Brightness)
@@ -160,42 +163,42 @@ void SeeedOLED::setBrightness(unsigned char Brightness)
 void SeeedOLED::setHorizontalMode()
 {
     addressingMode = HORIZONTAL_MODE;
-    sendCommand(0x20); 			//set addressing mode
-    sendCommand(0x00); 			//set horizontal addressing mode
+    sendCommand(0x20);          //set addressing mode
+    sendCommand(0x00);          //set horizontal addressing mode
 }
 
 void SeeedOLED::setPageMode()
 {
     addressingMode = PAGE_MODE;
-    sendCommand(0x20); 			//set addressing mode
-    sendCommand(0x02); 			//set page addressing mode
+    sendCommand(0x20);          //set addressing mode
+    sendCommand(0x02);          //set page addressing mode
 }
 
 
 void SeeedOLED::setTextXY(unsigned char Row, unsigned char Column)
 {
-    sendCommand(0xB0 + Row); 			//set page address
-    sendCommand(0x00 + (8*Column & 0x0F)); 	//set column lower address
-    sendCommand(0x10 + ((8*Column>>4)&0x0F)); 	//set column higher address
+    sendCommand(0xB0 + Row);            //set page address
+    sendCommand(0x00 + (8*Column & 0x0F));  //set column lower address
+    sendCommand(0x10 + ((8*Column>>4)&0x0F));   //set column higher address
 }
 
 
 void SeeedOLED::clearDisplay()
 {
   unsigned char i,j;
-  sendCommand(SeeedOLED_Display_Off_Cmd); 	//display off
+  sendCommand(SeeedOLED_Display_Off_Cmd);   //display off
   for(j=0;j<8;j++)
-  {	
-    setTextXY(j,0);    
+  {
+    setTextXY(j,0);
     {
       for(i=0;i<16;i++)  //clear all columns
       {
-        putChar(' ');    
+        putChar(' ');
       }
     }
   }
-  sendCommand(SeeedOLED_Display_On_Cmd); 	//display on
-  setTextXY(0,0);    
+  sendCommand(SeeedOLED_Display_On_Cmd);    //display on
+  setTextXY(0,0);
 }
 
 void SeeedOLED::sendData(unsigned char Data)
@@ -216,7 +219,7 @@ void SeeedOLED::putChar(unsigned char C)
     if(C < 32 || C > 127) //Ignore non-printable ASCII characters. This can be modified for multilingual font.
     {
     C=' '; //Space
-    }	
+    }
     unsigned char i=0;
     for(i=0;i<8;i++)
     {
@@ -230,7 +233,7 @@ void SeeedOLED::putString(const char *String)
     unsigned char i=0;
     while(String[i])
     {
-        putChar(String[i]);     
+        putChar(String[i]);
         i++;
     }
 }
@@ -241,20 +244,20 @@ unsigned char SeeedOLED::putNumber(long long_num)
   unsigned char i = 0;
   unsigned char f = 0;
 
-  if (long_num < 0) 
+  if (long_num < 0)
   {
     f=1;
     putChar('-');
     long_num = -long_num;
-  } 
-  else if (long_num == 0) 
+  }
+  else if (long_num == 0)
   {
     f=1;
     putChar('0');
     return f;
-  } 
+  }
 
-  while (long_num > 0) 
+  while (long_num > 0)
   {
     char_buffer[i++] = long_num % 10;
     long_num /= 10;
@@ -294,8 +297,8 @@ unsigned char SeeedOLED::putFloat(float floatNumber,unsigned char decimal)
     putChar('.');
     f +=1;
  }
-  decy = floatNumber-temp;//decimal part, 
-  for(unsigned char i=0;i<decimal;i++)//4 
+  decy = floatNumber-temp;//decimal part,
+  for(unsigned char i=0;i<decimal;i++)//4
   {
     decy *=10;// for the next decimal
     temp = decy;//get the decimal
@@ -331,8 +334,8 @@ unsigned char SeeedOLED::putFloat(float floatNumber)
     putChar('.');
     f +=1;
  }
-  decy = floatNumber-temp;//decimal part, 
-  for(unsigned char i=0;i<decimal;i++)//4 
+  decy = floatNumber-temp;//decimal part,
+  for(unsigned char i=0;i<decimal;i++)//4
   {
     decy *=10;// for the next decimal
     temp = decy;//get the decimal
@@ -348,7 +351,7 @@ void SeeedOLED::drawBitmap(unsigned char *bitmaparray,int bytes)
   char localAddressMode = addressingMode;
   if(addressingMode != HORIZONTAL_MODE)
   {
-      //Bitmap is drawn in horizontal mode 	
+      //Bitmap is drawn in horizontal mode
       setHorizontalMode();
   }
 
@@ -360,25 +363,25 @@ void SeeedOLED::drawBitmap(unsigned char *bitmaparray,int bytes)
   if(localAddressMode == PAGE_MODE)
   {
      //If pageMode was used earlier, restore it.
-     setPageMode(); 
+     setPageMode();
   }
   
 }
 
-void SeeedOLED::setHorizontalScrollProperties(bool direction,unsigned char startPage, unsigned char endPage, unsigned char scrollSpeed)
+void SeeedOLED::setHorizontalScrollProperties(unsigned char direction, unsigned char startPage, unsigned char endPage, unsigned char scrollSpeed)
 {
 /*
 Use the following defines for 'direction' :
 
- Scroll_Left			
- Scroll_Right			
+ Scroll_Left
+ Scroll_Right
 
 Use the following defines for 'scrollSpeed' :
 
- Scroll_2Frames		
+ Scroll_2Frames
  Scroll_3Frames
  Scroll_4Frames
- Scroll_5Frames	
+ Scroll_5Frames
  Scroll_25Frames
  Scroll_64Frames
  Scroll_128Frames
@@ -393,7 +396,7 @@ Use the following defines for 'scrollSpeed' :
    }
    else
    {
-        //Scroll Left  
+        //Scroll Left
         sendCommand(0x27);
 
    }
@@ -427,4 +430,3 @@ void SeeedOLED::setInverseDisplay()
 
 
 SeeedOLED SeeedOled;  // Preinstantiate Objects
-
