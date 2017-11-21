e8c5fc2891a9b85b306a5c37489916ba91d8afc4
Olof Astrand <olof.astrand@gmail.com>
2017-11-22 00:56:31 +0100

    Silly example

--- examples/27_wifi_matrix/main/matrixdemo.cpp
+++ examples/27_wifi_matrix/main/matrixdemo.cpp
@@ -36,8 +36,7 @@ void loop() {
     delay(300);
     int ix=0;
 
-  #if 0
-    for (ix=0; ix<8*8*4*2; ix++) {
+    for (ix=0; ix<256; ix++) {
         matrix.simplePixel(ix,1);
         matrix.writeScreen();
         delay(20);
@@ -47,12 +46,11 @@ void loop() {
         }
     }
 
-    for (ix=0; ix<8*8*4*2; ix++) {
+    for (ix=0; ix<256; ix++) {
         matrix.simplePixel(ix,0);
         matrix.writeScreen();
         
     }
-  #endif
 
     // blink!
     //matrix.blink(true);
@@ -109,3 +107,100 @@ void loop() {
     
   }
 }
+
+#if 0
+//
+
+#include "Arduino.h"
+
+// HT1632C PINs
+
+#define DISPLAY_CS    23
+#define DISPLAY_WR    21
+#define DISPLAY_DATA  22
+
+
+// HT1632C Commands
+
+#define HT1632C_READ    B00000110
+#define HT1632C_WRITE   B00000101
+#define HT1632C_CMD     B00000100
+
+#define HT1632_CMD_SYSON  0x01
+#define HT1632_CMD_LEDON  0x03
+
+void ht1632c_send_command(byte command);
+void ht1632c_send_bits(byte bits, byte firstbit);
+
+// ----------------------------------------
+//  HT1632C functions
+// ----------------------------------------
+
+void ht1632c_send_command(byte command) {
+  
+  digitalWrite(DISPLAY_CS, LOW);  
+  ht1632c_send_bits(HT1632C_CMD, 1 << 2);
+  ht1632c_send_bits(command, 1 << 7);
+  ht1632c_send_bits(0, 1);
+  digitalWrite(DISPLAY_CS, HIGH); 
+}
+
+void ht1632c_send_bits(byte bits, byte firstbit) {
+  
+  while(firstbit) {
+    digitalWrite(DISPLAY_WR, LOW);
+    if (bits & firstbit) digitalWrite(DISPLAY_DATA, HIGH);
+    else digitalWrite(DISPLAY_DATA, LOW);
+    digitalWrite(DISPLAY_WR, HIGH);
+    firstbit >>= 1;
+  }
+}
+
+
+int j;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  // All PINs are output
+  pinMode(DISPLAY_CS, OUTPUT);
+  pinMode(DISPLAY_WR, OUTPUT);
+  pinMode(DISPLAY_DATA, OUTPUT);
+  
+  // Enable System oscillator and LED duty cycle generator
+  ht1632c_send_command(HT1632_CMD_SYSON);
+  ht1632c_send_command(HT1632_CMD_LEDON);
+  
+  j = 0;
+}
+
+void loop() {
+
+  // select display
+  digitalWrite(DISPLAY_CS, LOW);  
+ 
+  // send WRITE command
+  ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+  
+  // send start address, 00h
+  ht1632c_send_bits(0x00, 1 << 6);
+
+  // send data
+  for(int i = 0; i < 256; i++) {
+    digitalWrite(DISPLAY_WR, LOW);
+    if(i == j) digitalWrite(DISPLAY_DATA, HIGH);
+    else digitalWrite(DISPLAY_DATA, LOW);
+    digitalWrite(DISPLAY_WR, HIGH);    
+  }
+  
+  // unselect display
+  digitalWrite(DISPLAY_CS, HIGH);  
+
+  // cycle LED position to be turned on
+  if(j < 256 ) j++;
+  else j = 0;
+  
+  delay(20);
+}
+#endif
