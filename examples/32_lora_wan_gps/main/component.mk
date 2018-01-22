97096a6fa9378da0c47a2bd0c9f61f5a94b088f0
Olof Astrand <olof.astrand@gmail.com>
2018-01-22 17:37:51 +0000

    Added compilation info

--- examples/32_lora_wan_gps/main/component.mk
+++ examples/32_lora_wan_gps/main/component.mk
@@ -6,3 +6,9 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
+
+#include $(IDF_PATH)/make/component_common.mk
+#COMPONENT_ADD_INCLUDEDIRS:= .
+#COMPONENT_SRCDIRS := .
+CXXFLAGS += -fno-rtti
+
